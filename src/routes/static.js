0309227524f4da9d96b8361a566aeed5bf35ca3d
Lindsay Spencer <43455549+lindsayspencer@users.noreply.github.com>
2019-01-21 11:56:48 -0600

    completed assignment: express

--- src/routes/static.js
+++ src/routes/static.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.send("Welcome to Bloccit");
+    next();
+})
+
+router.get('/marco', (req, res, next) => {
+    res.send("polo");
 })
 
 module.exports = router;
