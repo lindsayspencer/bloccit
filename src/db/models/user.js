4bc701c167c3bd7130d9c644555f8fc09e8584a3
Lindsay Spencer <43455549+lindsayspencer@users.noreply.github.com>
2019-03-31 14:11:58 -0500

    completed checkpoint favoriting

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -36,7 +36,12 @@ module.exports = (sequelize, DataTypes) => {
     User.hasMany(models.Vote, {
       foreignKey: "userId",
       as: "votes"
-    })
+    });
+
+    User.hasMany(models.Favorite, {
+      foreignKey: "userId",
+      as: "favorites"
+    });
   };
   User.prototype.isAdmin = function() {
     return this.role === "admin";
